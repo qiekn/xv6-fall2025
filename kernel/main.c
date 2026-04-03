28a8e28e32d7c673d81d28337e58e35eb991d965
qiekn <qiekndev@gmail.com>
2026-04-03 14:33:52 +0800

    chore: format main.c

--- kernel/main.c
+++ kernel/main.c
@@ -7,9 +7,7 @@
 volatile static int started = 0;
 
 // start() jumps here in supervisor mode on all CPUs.
-void
-main()
-{
+void main() {
   if(cpuid() == 0){
     consoleinit();
     printfinit();
@@ -41,5 +39,5 @@ main()
     plicinithart();   // ask PLIC for device interrupts
   }
 
-  scheduler();        
+  scheduler();
 }
